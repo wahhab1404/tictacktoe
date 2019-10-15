b264315f96c884622ce3fa0680e85557e146d52b
wahhab1404 <a.wahhab1404@gmail.com>
2019-10-15 22:44:58 +0300

    TicTacToe v1.2

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
+
+let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // start clean game board
+
+
 const playerOne = 'O'; // first player
 const playerTow = 'x'; //second player
+//------------- Selectors
 const button = document.querySelector("button");
+let xWinCount = document.querySelector(".xWinScore"); // x win counter
+let oWinCount = document.querySelector(".oWinScore"); // o win counter 
+const roundTag = document.querySelector(".roundHead");// to use it in round counter 
+
 let counter = 0;
+let xscore = 0;
+let oscore = 0;
+let roundCounter = 1;
 
 
 
 let turn = document.querySelector(".turn");
 turn.innerText = " X Starts!"
 
-let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // start clean game board
 
-let xWinCount = document.querySelector(".xWinScore"); // x win counter
-let oWinCount = document.querySelector(".oWinScore"); // o win counter 
-let tieCount = document.querySelector(".tei"); 
-// let scoreXtitle = document.querySelector('.scorexTitle');
+
+
+// Adding round counter
+roundTag.innerText = "Round " + roundCounter
 
 let playerTurn = true; // determin playing player 
 const tr0 = document.querySelector("#tr0");
-const tr = document.querySelectorAll(".cl"); 
-const remove = function(){           //Remove eventlistener if any one win and call it in checkWin function
-    for (let i =0; i < tr.length ; i++) {
-        tr[i].removeEventListener('click',onClick)
+const tr = document.querySelectorAll(".cl");
+const remove = function () {           //Remove eventlistener if any one win and call it in checkWin function
+    for (let i = 0; i < tr.length; i++) {
+        tr[i].removeEventListener('click', onClick)
+    }
 }
-}
-const turnChange = function () { // Text the player turn 
+const turnChange = function () { // ANNOUNCE PLAYER TURN IN THE BOTTOM 
 
     if (playerTurn == false) {
-        turn.innerText = "Player O It is your Turn"
-        
+        turn.innerText = "O Turn"
+        turn.style.color = "blue"
+
     };
     if (playerTurn == true) {
-        turn.innerText = "Player X ... it is your Turn"
+        turn.innerText = " X Turn"
+        turn.style.color = "yellow"
         // scoreXtitle.style.backgroundColor="rgba()"
 
     };
@@ -39,110 +52,137 @@ const turnChange = function () { // Text the player turn
 
 const cel = document.querySelector('.cel');
 
+// wining conditions...
 const winCheck = function () { // wining conditions...
     if (gameBoard[0] == 1 && gameBoard[1] == 1 && gameBoard[2] == 1) {
-
-        oWinCount = oWinCount + 1
-        turn.innerText = "O WINS"
-        button.innerText= "Playe New Game"
+        turn.innerText = "O WINS" 
+        oscore++; 
+        button.innerText = "Playe New Game"
         document.body.style.backgroundImage = "green";
         remove()
-        
+
 
     }
     else if (gameBoard[3] == 1 && gameBoard[4] == 1 && gameBoard[5] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
+        oscore++;
+        button.innerText = "Playe New Game"
         remove()
 
     }
     else if (gameBoard[6] == 1 && gameBoard[7] == 1 && gameBoard[8] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
+        oscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (gameBoard[0] == 1 && gameBoard[3] == 1 && gameBoard[6] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
+        oscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (gameBoard[1] == 1 && gameBoard[4] == 1 && gameBoard[7] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
+        button.innerText = "Playe New Game"
+        oscore++;
         remove()
     }
     else if (gameBoard[2] == 1 && gameBoard[5] == 1 && gameBoard[8] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
-        remove()   
+        button.innerText = "Playe New Game"
+        oscore++;
+        remove()
     }
     else if (gameBoard[0] == 1 && gameBoard[4] == 1 && gameBoard[8] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
+        button.innerText = "Playe New Game"
+        oscore++;
         remove()
     }
     else if (gameBoard[2] == 1 && gameBoard[4] == 1 && gameBoard[6] == 1) {
         turn.innerText = "O WINS"
-        oWinCount = oWinCount + 1
+        oscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (gameBoard[0] == 2 && gameBoard[1] == 2 && gameBoard[2] == 2) {
-        turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
-        
+        turn.innerText = "X WINS";
+        xscore++;
+
+        turn.style.color = 'red'
+        roundCounter++;
+        button.innerText = "Playe New Game"
+
         remove()
     }
     else if (gameBoard[3] == 2 && gameBoard[4] == 2 && gameBoard[5] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        xscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (gameBoard[6] == 2 && gameBoard[7] == 2 && gameBoard[8] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        xscore++;
+        button.innerText = "Playe New Game"
         remove()
 
     }
     else if (gameBoard[0] == 2 && gameBoard[3] == 2 && gameBoard[6] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        xscore++;
+        button.innerText = "Playe New Game"
         remove()
 
     }
     else if (gameBoard[1] == 2 && gameBoard[4] == 2 && gameBoard[7] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        xscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (gameBoard[2] == 2 && gameBoard[5] == 2 && gameBoard[8] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        button.innerText = "Playe New Game"
+        xscore++;
         remove()
     }
     else if (gameBoard[0] == 2 && gameBoard[4] == 2 && gameBoard[8] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        xscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (gameBoard[2] == 2 && gameBoard[4] == 2 && gameBoard[6] == 2) {
         turn.innerText = "X WINS"
-        xWinCount = xWinCount + 1
+        xscore++;
+        button.innerText = "Playe New Game"
         remove()
     }
     else if (counter == 9) {
         console.log("Tie")
-        turn.innerText = "Tie .. play again PLEASE !!";
+        turn.innerText = "Tie ..WTF!!"
+        button.innerText = "Playe New Game"
+        
         remove()
     }
+    // STORE VALUES OF SCORS
+    xWinCount.innerText = xscore;
+    oWinCount.innerText = oscore;
     
+    
+
 }
 const onClick = function () { // change player turn
+    
     playerTurn = !playerTurn
     counter++;
     if (playerTurn == true) {
         this.innerText = playerOne;
-        this.style.backgroundColor = 'rgba(0, 230, 10)';// change color of cell
-        gameBoard[this.id.slice(-1)] = 1;
+        button.style.backgroundColor = "blue"
+        this.style.color = 'rgb(3, 18, 51)';// change color of cell
+        gameBoard[this.id.slice(-1)] = 1; // slice last num of ID 
 
 
 
@@ -150,7 +190,8 @@ const onClick = function () { // change player turn
     if (playerTurn == false) {
 
         this.innerText = playerTow;
-        this.style.backgroundColor = 'red';
+        this.style.color = 'rgb(227, 179, 7)';
+        button.style.backgroundColor = "blue"
         gameBoard[this.id.slice(-1)] = 2;
 
 
@@ -159,35 +200,42 @@ const onClick = function () { // change player turn
     winCheck();
     this.removeEventListener("click", onClick);
 
-    console.log(gameBoard);
 };
+
+//  RESET AND START GAME BUTTONS
 const restartBut = document.querySelector(".gameRestart");
+const buttonNew = document.querySelector(".newGame");
 
-const addEvents = function(){
-    for (let i =0; i < tr.length ; i++) {
-        tr[i].addEventListener('click',onClick)
+const addEvents = function () {
+    for (let i = 0; i < tr.length; i++) {
+        tr[i].addEventListener('click', onClick)
+    }
+
 }
+
+
+// RESTART / RESET THE GAME 
+    const gameRest = function () {
+        counter = 0;
+        // REST THE BOARD ARRAY 
+        gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+        
+            for (let i = 0; i < gameBoard.length; i++) {
+                // REST Game Board Color and TEXT
+                  tr[i].style.backgroundColor = ""
+                  tr[i].innerText = ""
+                // RESTOR EVENTLISTENERS
+                  tr[i].addEventListener('click', onClick)
+        // CHANGE START TURN EACH RESTART
+                 turnChange()
+            }
+        // ADD ROUND COUNTER AFTER EACH RESTART
+         roundCounter++;
+         roundTag.innerText = "Round " + roundCounter;
     
 }
 
-const gameRestart = function () {
+addEvents();
+// Restart Event
+restartBut.addEventListener('click', gameRest)
 
-    console.log("gameRestart works");
-
-    counter = 0;
-    gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        for (let i =0; i < gameBoard.length ; i++) {
-            tr[i].style.backgroundColor=""
-            tr[i].innerText=""
-            tr[i].addEventListener('click',onClick)
-            turnChange()
-                   }
-
-    
-   
-}
-   
-    addEvents();
-
-  
-    restartBut.addEventListener('click', gameRestart)
--- reademe.md
+++ reademe.md
@@ -1,4 +1,11 @@
 this is updates for project virstion.
+## Changes v1.2
+1- Clean up some codes.
+2- Add comments 
+3 - counters ( win counter and round counter ) are working now.
+4 - change layouts.
+5- Add responsive designe for the game (pc , iphones , ipads..)
+
 ## changes v 1.1
 1- Cleane up some codes.
 2- Add counter to win/tie for x/o "not functional yet"
